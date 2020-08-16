8e35e97cf06a51a033d905339cb24712e1b46617
Henry <huyearly@gmail.com>
2020-08-16 14:23:47 -0600

    Update dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import LoginForm from "./User/LoginForm";
 
 function App() {
   return (
     <div className="App">
-      <p /> "Just starting"
+      <LoginForm></LoginForm>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
 import React from "react";
 import App from "./App";
-import { mount } from "enzyme";
+import { mountm, shallow } from "enzyme";
 
 describe("Counter Testing", () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = mount(<App />);
+    wrapper = shallow(<App />);
   });
   test("renders App", () => {
     expect(wrapper.find("div").hasClass("App")).toBe(true);
